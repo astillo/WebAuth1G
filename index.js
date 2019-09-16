6a278e7baefb7f361b05c7eed5313130dbb5de57
astillo <clomes.alex@gmail.com>
2019-09-16 12:09:41 -0500

    login functional

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (bcrypt.compareSync(user.password, hash)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
